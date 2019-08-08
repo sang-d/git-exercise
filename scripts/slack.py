5a597eaa70bb17106aab6a6913868f49c7e0276e
Sang Dinh <sang.dinh@wizeline.com>
2019-08-08 16:05:25 +0700

    update circleci config

--- scripts/slack.py
+++ scripts/slack.py
@@ -1,24 +1,18 @@
 # API slackClient v1: https://github.com/slackapi/python-slackclient/tree/v1
 # API slackClient 2: https://slack.dev/python-slackclient/conversations.html#direct-messages
 
-import slack
 import os
+from slackclient import SlackClient
+
 print("start slack script")
 token = os.environ.get('slack_token')
 admin_user_id = os.environ.get('admin_user_id')
-client = slack.WebClient(token=token)
+
+sc = SlackClient(token)
+
+res = sc.api_call(  "chat.postMessage", channel=admin_user_id, text="Hi", as_user=True)
 
 
-# client.conversations_open(users=[myid])
-# response = client.chat_postMessage( channel='DF7MJ83EE', text="",as_user=True)
-
-# users = client.api_call("users.list")
-# mm = users.get("members")
-# for m in mm:
-# 	if m.get("profile").get("email") == "sang.dinh@wizeline.com":
-# 		print(m)
-
-
-# uinfo = client.api_call("users.info")
-
-# uu = client.api_call("users.lookupByEmail")
+response = sc.api_call("users.lookupByEmail", email="sang.dinh@wizeline.com")
+user = response.get('user')
+print(user.get('id'), user.get('name'))
